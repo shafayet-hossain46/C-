946ebdaa10832f8921dd3d7310a26fd712385bd1
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-08-28 20:08:39 +0600

    first

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amountOne;
+    cin>>amountOne;
+
+    int amountTwo;
+    cin>>amountTwo;
+
+    int sum = amountOne + amountTwo;
+    cout<<sum;
+    return 0;
+}
