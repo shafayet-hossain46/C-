2fb43c4acfc389b85807cfe09d0a216934c44aa9
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-10-12 20:24:39 +0600

    Added Ambiguity Resolution

---
+++ Harry/OOP/Inheritance/AmbiguityResolution.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Base1{
+    public:
+        void greet(){
+            cout<<"How are you?"<<endl;
+        }
+};
+
+class Base2{
+    public:
+        void greet()
+        {
+            cout << "Kaise ho?" << endl;
+        }
+};
+
+class Derived : public Base1, public Base2{
+   int a;
+   public:
+    void greet(){
+        Base2 :: greet();
+    }
+};
+int main(){
+  // Ambibuity 1
+     Derived d;
+     d.greet();
+    return 0;
+}
