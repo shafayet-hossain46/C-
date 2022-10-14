97fe0574621aae700101cf4dfec17e7b8f29e18f
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-10-14 19:53:38 +0600

    Added virtual function to solve the issue

--- Harry/OOP/Polymorphism/VirtualFunction.cpp
+++ Harry/OOP/Polymorphism/VirtualFunction.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Base{
     public:
         int varBase;
-        void displayData(){
+        virtual void displayData(){ // Virtual function is an example of runtime polymorphism.
             cout<<"Base Class Var Value Is: "<<varBase<<endl;
         }
 
@@ -12,7 +12,7 @@ class Base{
 
 class Derived : public Base{
     public:
-        int varDerived;
+        int varDerived = 50;
         void displayData(){
             cout<<"Base Class Var Value Is: "<<varBase<<endl;
             cout<<"Derived Class Var Value Is: "<<varDerived<<endl;
@@ -24,20 +24,10 @@ int main(){
     Base base_obj;
     Derived derived_obj;
 
-    /*
-        When base class pointer points derived class object still the pointer will be pointing to base class public
-        members, because base class in not inherited. On the other hand, when derived class pointer is made it has
-        the access to the public members of the base class, because, it inherits the public members from the base class.
-    */
+    
     base_class_pointer = &derived_obj; // Base class's pointer is pointing Derived class object.
     base_class_pointer-> varBase = 30;
     base_class_pointer-> displayData();
-
-    Derived * derived_class_pointer;
-    derived_class_pointer = &derived_obj;
-    derived_class_pointer -> varBase = 60;
-    derived_class_pointer -> varDerived = 50;
-    derived_class_pointer->displayData();
 
     return 0;
 }
