86eb76f046a65e1cb052bcd030099b3f141c84d6
shafayet-hossain46 <shafayethossaing46@gmail.com>
2022-10-15 12:03:44 +0600

    Added default template variable typr

--- Harry/OOP/Templates_C++/Template.cpp
+++ Harry/OOP/Templates_C++/Template.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 
-template<class T1, class T2>
+// template<class T1=int, class T2=float, class T3=char> // Default type can be set
+template<class T1, class T2, class T3>
 class myClass{
     public:
         T1 data1;
@@ -10,14 +11,16 @@ class myClass{
             data1 = a;
             data2 = b;
         }
-        void display(){
+        T3 display(){
             cout<<this->data1<<this->data2<<endl;
             // cout<<data1<<data2<<endl;
         }
 };
 
 int main(){
-    myClass<int, char> obj(5,'c');
+    // myClass<> obj(5,'c'); // For default type
+    // myClass<char, float, int> obj(5,'c'); // We can also change default type from here if needed.
+    myClass<int, char, void> obj(5,'c');
     obj.display();
     return 0;
 }
